fc87caef17bd04a06e724ed85a3d233938b429ed
saisasikumar <sasisai8341@gmail.com>
2025-03-22 23:38:51 +0530

    intial commit

---
+++ src/main/java/com/example/demo/FamilyPlanes1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyPlanes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyPlanes1Application.class, args);
+	}
+
+}
